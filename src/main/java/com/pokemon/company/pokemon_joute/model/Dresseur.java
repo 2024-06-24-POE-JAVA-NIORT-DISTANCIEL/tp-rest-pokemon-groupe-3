afaffa184f8bb6e03982a1c03606d0bcf18894c1
DPOsengo <157788890+DPOsengo@users.noreply.github.com>
2024-08-29 00:37:33 +0200

    ajout du fichier pour construire la base de données, amélioration du squelette du projet + premiers codes

--- src/main/java/com/pokemon/company/pokemon_joute/model/Dresseur.java
+++ src/main/java/com/pokemon/company/pokemon_joute/model/Dresseur.java
@@ -1,4 +1,73 @@
 package com.pokemon.company.pokemon_joute.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Dresseur {
+
+    // attributs
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true)
+    private String pseudo;
+
+    private String motDePasse;
+    private Integer portefeuille;
+    private List<Objet> inventaire;
+
+
+    // constructeurs
+    // laisser vide sauf cas particulier,
+    // dans ce cas, ajouter le constructeur par défaut sans paramètres
+
+
+    // getters et setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public Integer getPortefeuille() {
+        return portefeuille;
+    }
+
+    public void setPortefeuille(Integer portefeuille) {
+        this.portefeuille = portefeuille;
+    }
+
+    public List<Objet> getInventaire() {
+        return inventaire;
+    }
+
+    public void setInventaire(List<Objet> inventaire) {
+        this.inventaire = inventaire;
+    }
+
+    // méthodes (si nécessaire)
+
 }
--- src/main/java/com/pokemon/company/pokemon_joute/model/Objet.java
+++ src/main/java/com/pokemon/company/pokemon_joute/model/Objet.java
@@ -1,4 +1,64 @@
 package com.pokemon.company.pokemon_joute.model;
 
+import com.pokemon.company.pokemon_joute.utils.TypeObjet;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Objet {
+
+    // attributs
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private Integer prix;
+    private TypeObjet type;
+
+
+    // constructeurs
+    // laisser vide sauf cas particulier,
+    // dans ce cas, ajouter le constructeur par défaut sans paramètres
+
+
+    // getters et setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Integer prix) {
+        this.prix = prix;
+    }
+
+    public TypeObjet getType() {
+        return type;
+    }
+
+    public void setType(TypeObjet type) {
+        this.type = type;
+    }
+
+    // méthodes (si nécessaire)
+
 }
