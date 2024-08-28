1ed00428aae4a7b39d380396aafb255962d44293
DPOsengo <157788890+DPOsengo@users.noreply.github.com>
2024-08-28 11:45:15 +0200

    premier commit

---
+++ src/main/java/com/pokemon/company/pokemon_joute/PokemonJouteApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.company.pokemon_joute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonJouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonJouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemon/company/pokemon_joute/PokemonJouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.company.pokemon_joute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonJouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
