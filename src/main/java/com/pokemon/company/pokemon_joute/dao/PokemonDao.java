2daadcf4c671f69c7335e2a9feb86b43dbc592df
Carl <karl.knezic@gmail.com>
2024-08-29 12:25:04 +0200

    Ajout de la classe pokémon avec le dao, service, restController, classe de test

--- src/main/java/com/pokemon/company/pokemon_joute/dao/PokemonDao.java
+++ src/main/java/com/pokemon/company/pokemon_joute/dao/PokemonDao.java
@@ -4,6 +4,11 @@ import com.pokemon.company.pokemon_joute.model.Pokemon;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PokemonDao extends CrudRepository<Pokemon, Long> {
+
+    List<Pokemon> findByNomContainingIgnoreCaseOrderByNomDesc(String nom);
+
 }
--- src/main/java/com/pokemon/company/pokemon_joute/model/Pokemon.java
+++ src/main/java/com/pokemon/company/pokemon_joute/model/Pokemon.java
@@ -1,4 +1,79 @@
 package com.pokemon.company.pokemon_joute.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private int niveau;
+    private int experience;
+    private int pv;
+    private int pvMax;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public int getPvMax() {
+        return pvMax;
+    }
+
+    public void setPvMax(int pvMax) {
+        this.pvMax = pvMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon : {" +
+                "id = " + id +
+                ", nom = " + nom + '\'' +
+                ", niveau = " + niveau +
+                ", experience = " + experience +
+                ", pv = " + pv +
+                ", pvMax = " + pvMax +
+                '}';
+    }
 }
