2788ac9cd8eb677519d3170a02550bb045f7e82c
servlet-groupe-2 <thierry.monprofit@gmail.com>
2024-08-29 09:30:49 +0200

    premier push des classes Espece, EspeceService, EspeceRestController et EspeceDao

--- src/main/java/com/pokemon/company/pokemon_joute/model/Espece.java
+++ src/main/java/com/pokemon/company/pokemon_joute/model/Espece.java
@@ -1,4 +1,63 @@
 package com.pokemon.company.pokemon_joute.model;
 
+import com.pokemon.company.pokemon_joute.utils.EnumType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Espece {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nom;
+	
+	private EnumType type;
+	
+	private Integer pvInitial;
+	
+	//private Attaque attaqueInitiale;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public EnumType getType() {
+		return type;
+	}
+
+	public void setType(EnumType type) {
+		this.type = type;
+	}
+
+	public Integer getPvInitial() {
+		return pvInitial;
+	}
+
+	public void setPvInitial(Integer pvInitial) {
+		this.pvInitial = pvInitial;
+	}
+
+
+
+
+
+	
+	
 }
---
+++ src/main/java/com/pokemon/company/pokemon_joute/utils/EnumType.java
@@ -0,0 +1,8 @@
+package com.pokemon.company.pokemon_joute.utils;
+
+public enum EnumType {
+	
+	VAL1,
+	VAL2
+
+}
