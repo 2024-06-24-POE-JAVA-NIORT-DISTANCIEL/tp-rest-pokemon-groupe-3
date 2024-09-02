f2dbd593b1277de4142205c20d19314d25e57294
Carl <karl.knezic@gmail.com>
2024-09-02 09:52:38 +0200

    message de commit

--- src/main/java/com/pokemon/company/pokemon_joute/model/Dresseur.java
+++ src/main/java/com/pokemon/company/pokemon_joute/model/Dresseur.java
@@ -19,7 +19,7 @@ public class Dresseur {
 
     private String motDePasse;
 
-    private Integer portefeuille = 100; // ici le choix d'Integer plutôt que int reflète le nullable
+    private Integer portefeuille = 100; // ici le choix d'Integer plutôt qu'un int reflète le nullable
 
     @OneToMany(mappedBy = "dresseur")
     private List<Pokemon> pokemons;
